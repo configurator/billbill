fe32754d36d2460eeb8de3d8968bd5d6275ad168
Dor Kleiman <configurator@gmail.com>
2013-10-01 19:35:55 -0400

    configure require.js in index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
-define(function (require) {
-    require.config({
-        paths: {
-            'google-drive-client': 'https://apis.google.com/js/client.js'
-        }
-    });
-    
-    require(['drive'], function () {
-        console.log('Everything loaded.');
-    });
+define(['require', 'drive'], function (require, drive) {
+    console.log('Everything loaded.');
 });
